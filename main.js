8f6bf28026060368ee5e5eb80babeacbf26576fb
ChloePark85 <hapark85@gmail.com>
2022-11-27 22:41:49 +0900

    added main category

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 document.querySelector("#host-suggest").addEventListener("mouseover", function(){
-    document.querySelector("#host-suggest").style.backgroundColor="#dddddd";
+    document.querySelector("#host-suggest").style.backgroundColor= "#f7f7f7"
+})
+
+document.querySelector("#host-suggest").addEventListener("mouseout", function(){
+    document.querySelector("#host-suggest").style.backgroundColor="white";
 })
